a2b3946350cf7964e4bf456d80777a34b249ca44
Eric Clark <eclark0013@gmail.com>
2019-07-06 14:18:51 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+def display_rainbow(arugment)
+
+    puts "R: #{arugment[0]}, O: #{arugment[1]}, Y: #{arugment[2]}, G: #{arugment[3]}, B: #{arugment[4]}, I: #{arugment[5]}, V: #{arugment[6]}"
+
+end
