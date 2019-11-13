993310f88ae098aae0b290e8d9e6c2df8c26f2eb
Alethea Cary <aletheacary@gmail.com>
2019-11-13 12:05:46 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,14 @@
 # Write your #display_rainbow method here
+require 'pry'
+def display_rainbow(colors)
+  counter = 1
+  colors.each do |color|
+    if (counter) == colors.length
+      print "#{color.split(//)[0].upcase}: #{color}"
+    else
+      print "#{color.split(//)[0].upcase}: #{color}, "
+      counter +=1
+    end
+
+  end
+end
