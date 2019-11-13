691a48b27770ac88f6b308063032b0d616a21299
Alethea Cary <aletheacary@gmail.com>
2019-11-13 12:30:41 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,14 +1,14 @@
 # Write your #display_rainbow method here
 require 'pry'
 def display_rainbow(colors)
-  counter = 1
-  colors.each do |color|
-    if (counter) == colors.length
-      print "#{color.split(//)[0].upcase}: #{color}"
-    else
-      print "#{color.split(//)[0].upcase}: #{color}, "
-      counter +=1
-    end
-
-  end
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+  # counter = 1
+  # colors.each do |color|
+    # if (counter) == colors.length
+    #   print "#{color.split(//)[0].upcase}: #{color}"
+    # else
+    #    print "#{color.split(//)[0].upcase}: #{color}, "
+    #   counter +=1
+    # end
+  # end
 end
