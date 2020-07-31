16aeb316c6769e5d72f541880571cfd11613f808
Clint Richards <richardsclint17@gmail.com>
2020-07-31 14:43:02 +0000

    Done.

---
+++ print_business_card.rb
@@ -0,0 +1,9 @@
+def print_business_card(person)
+  bob     = ["Bob", 46, "Juggler", "555-555-5555"]
+  stefani = ["Stefani", 49, "Firefighter"]
+
+  puts "Name: #{bob[0]}, Age: #{bob[1]}, Occupation: #{bob[2]}"
+  puts "Name: #{stefani[0]}, Age: #{stefani[1]}, Occupation: #{stefani[2]}"
+  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts "Contact: #{person[3]}"
+end
