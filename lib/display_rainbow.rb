0beeaca1fe59f0b76e5fa496f6ba43c3086b6062
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-08-19 03:56:40 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
-# Write your #display_rainbow method here
+def display_rainbow(array)
+  print "#{array[0][0].upcase}: #{array[0]}, #{array[1][0].upcase}: #{array[1]}, "
+  print "#{array[2][0].upcase}: #{array[2]}, #{array[3][0].upcase}: #{array[3]}, "
+  print "#{array[4][0].upcase}: #{array[4]}, #{array[5][0].upcase}: #{array[5]}, "
+  puts "#{array[6][0].upcase}: #{array[6]}"
+end
