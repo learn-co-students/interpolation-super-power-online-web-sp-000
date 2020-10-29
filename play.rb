7511e47c2fd25ec6ae1b57dcbbd25bfd49c23275
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-10-29 14:59:46 +0000

    Done.

---
+++ play.rb
@@ -0,0 +1,7 @@
+def print_business_card(person)
+  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+end
+
+display rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+
