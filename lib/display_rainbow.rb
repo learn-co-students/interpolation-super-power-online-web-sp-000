84cbf4e798155fa5af36faa92fb418c44e19a3bf
remote-torrent-6747 <abbyc@sky.com>
2020-11-19 20:11:09 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,4 @@ def display_rainbow(colours)
   puts colours
 end
 
-display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+#display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
