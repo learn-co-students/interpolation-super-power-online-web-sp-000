65ba52b448ac372fb42dbb6fc4f9fc9e5e88204e
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-04-29 22:25:26 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  colors = ["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n"]
+  colors = [R:'red', O:'orange', Y:'yellow', G:'green', B:'blue', I:'indigo', V:'violet']
+  puts "#{display_rainbow"
 end
